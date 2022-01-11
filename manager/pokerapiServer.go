0ff5c99c7e1c3398ff05f1784e26e585621d9310
shwon <shwon@ntels.com>
2022-01-11 17:59:22 +0900

    user login  추가

--- manager/pokerapiServer.go
+++ manager/pokerapiServer.go
@@ -6,15 +6,21 @@ import (
 	"os"
 )
 
+const api_game = "/api/porker/"
+const api_version = "v1"
+
 //api 추가
 const (
-	URI_USER_INFO  = "/api/poker/v1/user"       // POST : 가입 GET : 조회 (ID 찾기 또는 비번) PUT : 수정 DELETE : 탈퇴
-	URI_USER_LOGIN = "/api/poker/v1/user/login" // GET : 로그인 요청
+	// 회원 관련
+	URI_USER_INFO  = api_game + api_version + "/user/manage" // POST : 가입 GET : 조회 (ID 찾기 또는 비번) PUT : 수정 DELETE : 탈퇴
+	URI_USER_LOGIN = api_game + api_version + "/user/login"  // GET : 로그인 요청
 
+	// 게임관련
 )
 
 func OpenServer() {
 	http.HandleFunc(URI_USER_INFO, Handler_userInfo())
+	http.HandleFunc(URI_USER_INFO, Handler_login())
 
 	err := http.ListenAndServe(":44447", nil)
 	if err != nil {
--- manager/userManager.go
+++ manager/userManager.go
@@ -12,7 +12,27 @@ type User_Info struct {
 }
 
 func Handler_userInfo() func(http.ResponseWriter, *http.Request) {
-	return func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			//회원 저장.
+		case http.MethodPut:
+			//회원 수정.
+		case http.MethodDelete:
+			//회원 탈퇴.
+		case http.MethodGet:
+			//회원 조회.
+		default:
+			http.Error(w, "Invalid request method.", http.StatusBadRequest)
+		}
+	}
+}
 
+func Handler_login() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request){
+		switch r.Method {
+		case http.MethodGet:
+						
+		}
 	}
 }
