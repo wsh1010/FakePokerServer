13525ec547f4002d483e08678dcf6fe83da29e96
shwon <shwon@ntels.com>
2022-01-19 11:00:56 +0900

    ........ing

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"log"
-	"math"
-	"math/rand"
-	"strings"
-	"time"
 )
 
 func main() {
@@ -17,42 +13,4 @@ func main() {
 			main()
 		}
 	}()
-	compare()
-}
-
-func shuffle(r *rand.Rand) string {
-	cards := make([]int, 0)
-	for i := 0; i < 20; i++ {
-		cardnum := i/2 + 1
-		cards = append(cards, cardnum)
-	}
-
-	arr := []string{"1", "2", "3"}
-
-	for i := len(arr) - 1; i > 0; i-- {
-		j := int(math.Floor(r.Float64() * float64(i+1)))
-
-		temp := arr[i]
-		arr[i] = arr[j]
-		arr[j] = temp
-	}
-	result := strings.Join(arr, "")
-	return result
-}
-
-func compare() {
-	results := make(map[string]int)
-	timeSource := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(timeSource)
-	for i := 0; i < 1000000; i++ {
-		result := shuffle(random)
-		if _, exist := results[result]; !exist {
-			results[result] = 1
-		} else {
-			results[result]++
-		}
-	}
-	for key, value := range results {
-		log.Println(key, " : ", value)
-	}
 }
--- manager/gameManager.go
+++ manager/gameManager.go
@@ -1,7 +1,13 @@
 package manager
 
 import (
+	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func Handler_Game_Ready() func(http.ResponseWriter, *http.Request) {
@@ -28,4 +34,30 @@ func Handler_Game_Endturn() func(http.ResponseWriter, *http.Request) {
 
 		}
 	}
+}
+
+func shuffle(r *rand.Rand) []string {
+	cards := make([]string, 0)
+	for i := 0; i < 20; i++ {
+		card_num := i/2 + 1
+		cards = append(cards, strconv.Itoa(card_num))
+	}
+
+	for i := len(cards) - 1; i > 0; i-- {
+		j := int(math.Floor(r.Float64() * float64(i+1)))
+
+		temp := cards[i]
+		cards[i] = cards[j]
+		cards[j] = temp
+	}
+	return cards
+}
+
+func forgame() {
+	timeSource := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(timeSource)
+	card := shuffle(random)
+	dbCard := strings.Join(card, "-")
+	newCard := strings.Split(dbCard, "-")
+	log.Print(newCard)
 }
